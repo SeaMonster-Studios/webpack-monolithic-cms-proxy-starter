89507d3226ef7d718771a2da241c37442faf9156
Logan Call <logancall.dev@gmail.com>
2018-06-01 16:03:57 -0600

    Add flow, eslint

--- wp-webpack-scripts.php
+++ wp-webpack-scripts.php
@@ -9,18 +9,18 @@ function load_scripts_init() {
     // Use this if you have assets that need to be outside of the webpack bundle
     // $distStatic = get_stylesheet_directory_uri() . '/dist-static/';
 
-    wp_deregister_script('jquery');
-
-    wp_register_script('jquery', ('https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'), false, '1.10.2', false);
+    // Use these if you need a different version of jquery than what comes with WordPress
+    // wp_deregister_script('jquery');
+    // wp_register_script('jquery', ('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'), false, false, false);
+    // wp_enqueue_script('jquery');  
 
     // Use this if you have assets that need to be outside of the webpack bundle
     // wp_register_script('asset-fuke.js', $distStatic . 'asset-file.js', 'jquery', '1.0', true);
+    // wp_enqueue_script('asset-fuke.js');
     
-    wp_register_script('main.js', $dist . 'main.js', array('jquery', 'slidebars-js'), false, false);
+    wp_register_script('main.js', $dist . 'main.js', array(), false, false);
     wp_register_style('style.css', $dist . 'style.css');
 
-    wp_enqueue_script('jquery');  
-    // wp_enqueue_script('asset-fuke.js');
     wp_enqueue_script('main.js');
     wp_enqueue_style('style.css');
 }  
